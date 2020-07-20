694dea8e8e54b047bbdc1923a9778273875e9681
despraz <jeremie.despraz@epfl.ch>
2020-07-20 23:54:57 +0200

    (1) adding button to remove all groceries (2) security addition: only checked items can be erased (3) minor bugfix

--- remove_grocery.php
+++ remove_grocery.php
@@ -1,14 +1,19 @@
 <?php
 $item = $_GET['item'];
+$how = $_GET['how'];
 
 $con = mysqli_connect('localhost','username','password','database');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-//prevent SQL injections
-$stmt = $con->prepare('DELETE FROM groceries WHERE name = ?');
-$stmt->bind_param('s',$item);
+if($how=='checked') {
+  $stmt = $con->prepare('DELETE FROM groceries WHERE priority = -1');
+} else {
+  //prevent SQL injections
+  $stmt = $con->prepare('DELETE FROM groceries WHERE name = ?');
+  $stmt->bind_param('s',$item);
+}
 $success = $stmt->execute();
 if (!$success) {
   echo "Error: " . $con->error;
